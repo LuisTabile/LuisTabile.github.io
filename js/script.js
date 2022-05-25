717be0da35faf73d0dec9cca43ef6034eabfa5db
LuisTabile <LuisTabile@github.com>
2022-05-25 15:12:01 -0300

    remove some comments and add map

--- js/script.js
+++ js/script.js
@@ -36,3 +36,22 @@ $(document).ready(function(){
     });
   
   });
+
+  // Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  -28.38808850208791, -53.92725644828457
+  const uluru = { lat: -28.3880885, lng: -53.927256 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 6,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
